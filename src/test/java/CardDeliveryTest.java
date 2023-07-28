692afe6192a4b0c198cb9676fae2dbff508de8ca
Netology <orjuice@mail.ru>
2023-07-28 21:40:11 +0300

    Test failed

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,23 +1,36 @@
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
 import java.time.Duration;
-import static com.codeborne.selenide.Condition.visible;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.SetValueOptions.withText;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Condition.exactText;
 
 public class CardDeliveryTest {
-    @Test
-    void shouldTestCardDeliveryForm() {
-        open("http://localhost:9999/");
-            $("[data-test-id = city] input").setValue("Владивосток"); //когда нужно значение вводить в поле, то пишем input
-            $("[data-test-id = date] input").setValue("02.08.2023");
-            $$(".input__control").first().setValue("Юлия Максимова");
-            $("[name=phone] input").setValue("+79166065820");
-            $("[data-test-id = agreement]").click();
-            $("button").click();
-            $(".notification").shouldHave(exactText ("Успешно! Ваша встреча успешно забронирована на 02.08.2023"), Duration.ofSeconds(10));
-       // $ (String.valueOf(withText("Встреча успешно забронирована"))).shouldBe(visible, Duration.ofSeconds(10));
+    private String generateDate (int addDays, String pattern) {//String, потому что мы в результате получаем строку с датой
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
-}
+    @Test
+    void shouldTestCardDeliveryForm () {
+        open("http://localhost:9999/");
+        $("[data-test-id = city] input").setValue("Влад");//когда нужно значение вводить в поле, то пишем input
+        $$(".menu-item__control").findBy(text ("Владивосток")).click();// не забыть кликнуть по городу
+        $("[data-test-id = date] input").sendKeys (Keys.chord(Keys.SHIFT, Keys.HOME, Keys.DELETE)); //удаляем текущую дату с сайта, кторая стоит по умолчанию
+       String currentDate = generateDate (4, "dd.MM.yyyy"); //задаем переменную с нужными параметрами
+        $("[data-test-id = date] input").sendKeys(currentDate); // заполняем поле "Дата" текущей датой. Использовать setValue не можем, тк это не хардкод
+        $("[data-test-id = name] input").setValue("Julia Максимова");
+        $("[data-test-id = phone] input").setValue("+79166065820");
+        $("[data-test-id = agreement]").click();
+        $(".button").click();
+        $(".notification__content")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                    .shouldHave(exactText("Встреча успешно забронирована на " + currentDate));
+    }
+
+    }
+
